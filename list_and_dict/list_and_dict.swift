d2dd1c197d1fabf77df1f202298004578910cdde
Kazuya Hatano <k28@me.com>
2019-08-07 22:36:18 +0900

    Add list and dictionary sample

---
+++ list_and_dict/list_and_dict.swift
@@ -0,0 +1,33 @@
+// 空の配列を初期化
+var emptylist = [String]()
+var emptydict = [String:String]()
+print(emptylist)
+print(emptydict)
+
+// 配列の初期化と追加
+var fruitslist = ["apple", "orange", "melon"]
+print(fruitslist)
+fruitslist[1] = "green apple"
+print(fruitslist)
+
+// 配列を順番に処理
+for (item) in fruitslist {
+    print(item)
+}
+
+
+// 辞書の初期化
+var occupations = [
+    "Malcolm" : "Captain",
+    "Kaylee"  : "Mechanic",
+]
+print(occupations)
+occupations["Jayne"] = "Public Relations"
+print(occupations)
+
+// 辞書を順番に処理
+for (key, value) in occupations {
+    print("\(key) is \(value)")
+}
+
+

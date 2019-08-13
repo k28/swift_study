1e8a91436ab8a77b5ae2603eeec361299cc0b2dd
Kazuya Hatano <k28@me.com>
2019-08-14 06:41:18 +0900

    add thread_sample (Initial Commit)

---
+++ thread_sample/thread_sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  thread_sample
+//
+//  Created by K.Hatano on 2019/08/14.
+//  Copyright © 2019 K.Hatano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

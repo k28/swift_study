617f570ec13a2c7c7c695c664279aeeacaab3724
Kazuya Hatano <k28@me.com>
2019-08-14 06:59:30 +0900

    add dispatch queue sample (MainQueue, GrobalQueue)

--- thread_sample/thread_sample/ViewController.swift
+++ thread_sample/thread_sample/ViewController.swift
@@ -12,9 +12,47 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        mainDispatchQueueSample()
+        grobalQueueSample()
     }
+    
+    /**
+     Mainディスパッチキューの使用サンプル
+     */
+    func mainDispatchQueueSample() {
+        let mainQueue = DispatchQueue.main
+        
+        // EXC_BAD_INSTRUCTION になる
+        // おそらく、MainスレッドからsyncでCallしたため、デッドロック
+        // mainQueue.sync {
+        //     print("Hello! sync MainQueue")
+        // }
+        
+        // 非同期で実行(おそらくこの処理が終わってからCallされる
+        mainQueue.async {
+            print("Hello! async MainQueue")
+        }
+    }
+    
+    func grobalQueueSample() {
+        let globalQueue = DispatchQueue.global(qos: .default)
+        
+        var greet = ""
+        
+        // 同期的に処理
+        globalQueue.sync {
+            greet = "Hello!"
+            print("\(greet) Grobal Sync Queue")
+        }
+        
+        // syncは同期的に処理されるので、スレッド処理の下で値にアクセスする事ができる
+        print("\(greet) grobalQueueSample")
 
+        globalQueue.async {
+            print("\(greet) Grobal Async Queue")
+        }
+    }
 
 }
 

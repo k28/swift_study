52b8399c9e4486e0941e89177626132a2c70d15f
Kazuya Hatano <k28@me.com>
2019-08-13 09:32:21 +0900

    Add http access sample (Initial Commit)

---
+++ http_access_sample/http_access_sample/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  http_access_sample
+//
+//  Created by kazuya on 2019/08/13.
+//  Copyright © 2019 K.Hatano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var valueLabel_: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        valueLabel_.text = "Hello World!"
+    }
+
+
+}
+

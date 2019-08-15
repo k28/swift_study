7ef173e01418f39ac24dd1a6dac3a7222835ce4d
Kazuya Hatano <k28@me.com>
2019-08-16 06:13:09 +0900

    Add worker_thread_sample (Initial Commit)

---
+++ worker_thread_sample/worker_thread_sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  worker_thread_sample
+//
+//  Created by kazuya on 2019/08/16.
+//  Copyright © 2019 K.Hatano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

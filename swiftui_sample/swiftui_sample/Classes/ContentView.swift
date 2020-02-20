c2dd851f41a6352c36a86a176a78e6361cf39456
Kazuya Hatano <k28@me.com>
2020-02-21 08:43:25 +0900

    add swift ui sample

---
+++ swiftui_sample/swiftui_sample/Classes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui_sample
+//
+//  Created by K.Hatano on 2020/02/21.
+//  Copyright © 2020 Kazuya. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

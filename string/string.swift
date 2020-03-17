dcb139280f77d1856fb269195c5ab85561820cf7
Kazuya Hatano <k28@me.com>
2020-03-18 05:37:19 +0900

    update

---
+++ string/string.swift
@@ -0,0 +1,16 @@
+
+// 特定の文字の場所で分割した部分文字列を取得する
+let word = "123e456"
+if let pos = word.firstIndex(of: "e") {
+  // Indexを使った文字列取得はRangeになるので[]で部分文字列を取得できる
+  _ = word[word.startIndex..<pos] // 123
+  _ = word[word.startIndex...pos] // 123e
+  _ = word[pos...]  // e456
+  // String.index(after:)でIndexのIndexを取得できる
+  _ = word[word.index(after: pos)...] // 456
+}
+
+// 文字で分割する
+_ = word.split(separator: "e")  // ["123", "456"]
+
+

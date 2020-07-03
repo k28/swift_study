5cc80da0f9e1c31576f00ad0c1d366162aa09618
Kazuya Hatano <k28@me.com>
2020-07-04 07:31:37 +0900

    update

---
+++ list_and_dict/dict_deep_change.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+// 辞書の中の深い部分を書き換える方法
+
+typealias DeepHash = [String: [String: [String]]]
+
+func changeDeepHash(_ dict: inout DeepHash) {
+    // Apple のcountを["100", "200", "300"]に変えたい
+
+    // 上から順に更新したものでアップデートしていく
+    if var apple = dict["Apple"] {
+        if var count = apple["count"] {
+            count.append("300")
+            apple["count"] = count
+        }
+        dict["Apple"] = apple
+    }
+}
+
+let original: DeepHash = [
+    "Apple":  ["color": ["red", "green"],    "count": ["100", "200"]],
+    "Orange": ["color": ["orange", "green"], "count": ["10", "20"]]
+    ]
+
+print(original)
+var copy = original
+changeDeepHash(&copy)
+
+print(original)
+print(copy)
